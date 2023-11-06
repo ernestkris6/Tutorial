0bc66c19609b782e37101f276527bc30c9fd972f
ERNEST CHRIS <ernestchris6@gmail.com>
2023-11-06 23:42:46 +0100

    filter

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,13 @@ const Home =() => {
       const [blogs, setBlogs] = useState([
         { title : "My new website", body : "Lorem ipsum...", author : "Mario", id : 1},
         { title : "Welcome party", body : "Lorem ipsum...", author : "Yoshi", id : 2},
-        { title : "Web dev top tips", body : "Lorem ipsum...", author : "Chris", id : 3}
+        { title : "Web dev top tips", body : "Lorem ipsum...", author : "Mario", id : 3}
       ])
 
   return(
     <div className='home'>
         <Blog blogs={blogs} title='All title!' />
+        <Blog blogs={blogs} title='Mario Blogs' blog={blogs.filter((blog)=> blog.author === 'Mario')}   />
     </div>
   )
 }
