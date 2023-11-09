512bda4569273f1bab3305e8eb3d0770c48bd3d1
ERNEST CHRIS <ernestchris6@gmail.com>
2023-11-09 17:06:54 +0100

    clear

--- src/Blog.js
+++ src/Blog.js
@@ -1,33 +1,4 @@
-// import React from 'react'
-
-// const Blog =({blogs, title, handleDelete}) => {
-
-//     // const blogs = props.blogs;
-//     // const title = props.title;
-
-//     // console.log(props, blogs);
-
- 
-
-//   return (
-//     <div  className='blog-list'>
-//         <h1>{title}</h1>
-//         {blogs.map((blog)=> (
-//             <div className='blog-preview' key={blog.id}>
-                
-//                 <h2>{blog.title}</h2>
-//                 <p><strong>{blog.body}</strong></p>
-//                 <p>written by {blog.author}</p>
-//                 <button onClick={()=> handleDelete(blog.id)}>Delete</button>
-//             </div>
-//           ))}
-//     </div>
-//   )
-// }
-
-// export default Blog;
-
-import React from 'react'
+import React from 'react';
 
 const Blog =({blogs, title, handleDelete}) => {
   // const blogs = props.blogs;
--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,3 @@
-// import React, {useEffect, useState} from 'react';
-// import Blog from './Blog';
-
-// const Home =() => {
-
-
-//       const [blogs, setBlogs] = useState([
-//         { title : "My new website", body : "Lorem ipsum...", author : "Mario", id : 1},
-//         { title : "Welcome party", body : "Lorem ipsum...", author : "Yoshi", id : 2},
-//         { title : "Web dev top tips", body : "Lorem ipsum...", author : "Mario", id : 3}
-//       ])
-
-      
-//   const handleDelete = (id) => {
-//     const newBlogs = blogs.filter(blog => blog.id !== id);
-//     setBlogs(newBlogs);
-// }
-
-
-//   useEffect(()=>{
-//     console.log('use efferct ran');
-//     console.log(blogs);
-//   })
-
-//   return(
-//     <div className='home'>
-//         <Blog blogs={blogs} title='All title!' handleDelete={handleDelete} />
-//         <Blog title='Mario Blogs!' handleDelete={handleDelete} blogs={blogs.filter((blog)=> blog.author === 'Mario')}   />
-//         <Blog title='Yoshi Blogs!' handleDelete={handleDelete} blogs={blogs.filter((blog)=> blog.author === 'Yoshi' )} />
-//     </div>
-//   )
-// }
-
-// export default Home;
-
 import React from 'react'
 import { useState } from 'react';
 
