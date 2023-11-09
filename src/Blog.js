deadeccffe62be1fe2aec190c472e4a448224bff
ERNEST CHRIS <ernestchris6@gmail.com>
2023-11-09 17:03:41 +0100

    retry

--- src/Blog.js
+++ src/Blog.js
@@ -1,26 +1,50 @@
-import React from 'react'
+// import React from 'react'
 
-const Blog =({blogs, title, handleDelete}) => {
+// const Blog =({blogs, title, handleDelete}) => {
 
-    // const blogs = props.blogs;
-    // const title = props.title;
+//     // const blogs = props.blogs;
+//     // const title = props.title;
 
-    // console.log(props, blogs);
+//     // console.log(props, blogs);
 
  
 
-  return (
-    <div  className='blog-list'>
-        <h1>{title}</h1>
-        {blogs.map((blog)=> (
-            <div className='blog-preview' key={blog.id}>
+//   return (
+//     <div  className='blog-list'>
+//         <h1>{title}</h1>
+//         {blogs.map((blog)=> (
+//             <div className='blog-preview' key={blog.id}>
                 
-                <h2>{blog.title}</h2>
-                <p><strong>{blog.body}</strong></p>
-                <p>written by {blog.author}</p>
-                <button onClick={()=> handleDelete(blog.id)}>Delete</button>
-            </div>
-          ))}
+//                 <h2>{blog.title}</h2>
+//                 <p><strong>{blog.body}</strong></p>
+//                 <p>written by {blog.author}</p>
+//                 <button onClick={()=> handleDelete(blog.id)}>Delete</button>
+//             </div>
+//           ))}
+//     </div>
+//   )
+// }
+
+// export default Blog;
+
+import React from 'react'
+
+const Blog =({blogs, title, handleDelete}) => {
+  // const blogs = props.blogs;
+  // const title =  props.title;
+
+  return (
+    <div className='blog-list'>
+        
+        <h1>{title}</h1>
+        {blogs.map((blog)=>(
+          <div className='blog-preview' key={blog.id}>
+          <h1>{blog.title}</h1>
+          <p>{blog.body}</p>
+          <p>{blog.author}</p>
+          <button onClick={()=> handleDelete(blog.id)}>Delete</button>
+          </div>
+        ))}
     </div>
   )
 }
--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,64 @@
-import React, {useEffect, useState} from 'react';
+// import React, {useEffect, useState} from 'react';
+// import Blog from './Blog';
+
+// const Home =() => {
+
+
+//       const [blogs, setBlogs] = useState([
+//         { title : "My new website", body : "Lorem ipsum...", author : "Mario", id : 1},
+//         { title : "Welcome party", body : "Lorem ipsum...", author : "Yoshi", id : 2},
+//         { title : "Web dev top tips", body : "Lorem ipsum...", author : "Mario", id : 3}
+//       ])
+
+      
+//   const handleDelete = (id) => {
+//     const newBlogs = blogs.filter(blog => blog.id !== id);
+//     setBlogs(newBlogs);
+// }
+
+
+//   useEffect(()=>{
+//     console.log('use efferct ran');
+//     console.log(blogs);
+//   })
+
+//   return(
+//     <div className='home'>
+//         <Blog blogs={blogs} title='All title!' handleDelete={handleDelete} />
+//         <Blog title='Mario Blogs!' handleDelete={handleDelete} blogs={blogs.filter((blog)=> blog.author === 'Mario')}   />
+//         <Blog title='Yoshi Blogs!' handleDelete={handleDelete} blogs={blogs.filter((blog)=> blog.author === 'Yoshi' )} />
+//     </div>
+//   )
+// }
+
+// export default Home;
+
+import React from 'react'
+import { useState } from 'react';
+
+import "./Blog";
 import Blog from './Blog';
 
 const Home =() => {
 
+    const [blogs, setBlogs] = useState([
+      { title: "Web Dev Tools", body: "lorem ipsum dolor...", author: "Ernest", id: "1" },
+      { title: "Learning React", body: "lorem ipsum dolor...", author: "Emma", id: "2"},
+      { title: "Learning Javascript", body: "lorem ipsum dolor...", author: "Ernest", id: "3"}
+    ])
 
-      const [blogs, setBlogs] = useState([
-        { title : "My new website", body : "Lorem ipsum...", author : "Mario", id : 1},
-        { title : "Welcome party", body : "Lorem ipsum...", author : "Yoshi", id : 2},
-        { title : "Web dev top tips", body : "Lorem ipsum...", author : "Mario", id : 3}
-      ])
 
-      
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-}
+    const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id );
+      setBlogs(newBlogs);
+    }
+   
 
-  useEffect(()=>{
-    console.log('use efferct ran');
-    console.log(blogs);
-  })
-
-  return(
+  return (
     <div className='home'>
-        <Blog blogs={blogs} title='All title!' handleDelete={handleDelete} />
-        <Blog blogs={blogs} title='Mario Blogs' handleDelete={handleDelete} blog={blogs.filter((blog)=> blog.author === 'Mario')}   />
+        <Blog blogs={blogs} title="Ernest's Blogs" handleDelete={handleDelete} />
+        <Blog blogs={blogs.filter(blog=> blog.author === 'Ernest')} title="Josh's Blogs" handleDelete={handleDelete} />
+        <Blog blogs={blogs.filter(blog => blog.author === 'Emma')} title="Emma's Blogs" handleDelete={handleDelete} />
     </div>
   )
 }
