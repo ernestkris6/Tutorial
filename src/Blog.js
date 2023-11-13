3963007756cde98d747c593b2d6fa4e7be7d92bd
ERNEST CHRIS <ernestchris6@gmail.com>
2023-11-13 23:18:34 +0100

    undo

--- src/Blog.js
+++ src/Blog.js
@@ -10,7 +10,7 @@ const Blog =({blogs, title}) => {
         <h1>{title}</h1>
         {blogs.map((blog)=>(
           <div className='blog-preview' key={blog.id}>
-          <Link to="/blogs/:id">
+          <Link to={`/blogs/${blog.id}`}>
           <h1>{blog.title}</h1>
           <p>{blog.body}</p>
           <p>written by {blog.author}</p>
