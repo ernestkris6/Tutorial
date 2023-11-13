52f19595aa971153fb5b21749a1403ebc6e71dc2
ERNEST CHRIS <ernestchris6@gmail.com>
2023-11-13 22:18:02 +0100

    blogdet

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,43 +1,26 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useFetch from './useFetch';
-import Blog from './Blog';
 
-const BlogDetails = () => {
+
+const BlogDetails = ({blog, title}) => {
 
     const { id } = useParams();
-  //   const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs' )
-  // return (
-  // //   <div>
-  // //       {isPending && <div><h2>Loading...</h2></div>}
-  // //       { error && <div><h2>{ error }</h2></div> }
-  // //       {blog && (
-  // //           <article>
-  // //               <h2>{ blog.title }</h2>
-  // //               <p>Written by { blog.author }</p>
-  // //               <div>{ blog.body }</div>
-
-  // //           </article>
-  // //       )}
-  // //   </div>
-  // // )
-
-  const {data:blog, isPending, error} = useFetch('http://localhost:8000/blogs' );
-
-
-
+    const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs' )
   return (
-    <div className='home'>
-          { error && <div>{error}</div>}
-          { isPending && <div><h2>Loading...</h2></div>}
-          { blog && (
+    <div>
+        {isPending && <div><h2>Loading...</h2></div>}
+        { error && <div><h2>{ error }</h2></div> }
+        {blog && (
             <article>
-              <h2>{ blog.title }</h2>
-              <p>Written by { blog.author }</p>
+                <h2>{ blog.title }</h2>
+                <p>Written by { blog.author }</p>
+                <div>{ blog.body }</div>
+
             </article>
-          )}
+        )}
     </div>
-  )
+  );
 }
 
 export default BlogDetails;
