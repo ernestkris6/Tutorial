c6be0bac21721bfed6497b239d220f93088364bc
ERNEST CHRIS <ernestchris6@gmail.com>
2023-11-16 11:03:08 +0100

    Deleting blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from './useFetch';
 
 
 const BlogDetails = () => {
 
+  
     const { id } = useParams();
-    const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id )
+    const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id );
+
+    const navigate = useNavigate()
+    const handleClick = () => {
+      fetch('http://localhost:8000/blogs/' + id, {
+        method: 'DELETE'
+      }).then(()=> {
+          navigate('/');
+      })
+    }
+  
   return (
     <div className='blog-details'>
       <h2 className='blog-details'>Blog - {id}</h2>
@@ -18,7 +29,7 @@ const BlogDetails = () => {
                 <p><b><i>Written by { blog.author }</i></b></p> <br></br>
                <div><h3>{ blog.body }</h3></div> 
                
-
+              <button onClick={handleClick}>DELETE</button>
             </article>
         )}
         
