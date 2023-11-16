84290e4b047f17de7749164f9c5f0685db7cba78
ERNEST CHRIS <ernestchris6@gmail.com>
2023-11-16 09:13:29 +0100

    prevent default

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return ( 
         <div className="create">
             <h2>
                 Add a New Blog</h2>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title</label>
                 <input 
                 type="text"
                 required
+                value={title}
+                onChange={(e)=> setTitle(e.target.value)}
                 />
 
             <label>Blog body</label>
@@ -23,7 +33,7 @@ const Create = () => {
                 </select>
 
                 <button>Add Blog</button>
-                
+                <p>{title}</p>
             </form>
         </div>
      );
